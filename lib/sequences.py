2ebac2b23e204c2051d4b5baa1193c47767d47d5
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-25 23:54:20 +0300

    passes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+def print_fibonacci(n):
+    """
+    Prints the Fibonacci sequence up to the specified length.
 
-def print_fibonacci(length):
-    pass
+    Args:
+        n (int): The length of the Fibonacci sequence to print.
+
+    Returns:
+        None
+    """
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence[:n])
